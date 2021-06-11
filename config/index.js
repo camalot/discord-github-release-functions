84d4609559fcd7d71e11a38382d8eeb814e1ccb1
Ryan Conrad <camalot@gmail.com>
2021-06-11 18:17:05 -0500

    initial commit

---
+++ config/index.js
@@ -0,0 +1,29 @@
+"use strict";
+const merge = require("merge");
+let outConfig = {
+  environment: process.env.NODE_ENV || ""
+};
+
+outConfig.isProduction = !(outConfig.environment === "dev" ||
+  outConfig.environment === "" ||
+  outConfig.environment === null ||
+  outConfig.environment === undefined ||
+  outConfig.environment === "development");
+
+
+const normalizedPath = require("path").join(__dirname, "./");
+require("fs")
+  .readdirSync(normalizedPath)
+  .forEach(function (file) {
+    var configMatch = /.*?\.config\.js/i;
+    if (
+      file !== "index.js" &&
+      file !== "config.js" &&
+      configMatch.test(file)
+    ) {
+      outConfig = merge(outConfig, require("./" + file));
+    }
+  });
+
+let result = merge({}, outConfig);
+module.exports = result;
